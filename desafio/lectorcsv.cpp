79fbd015e1ace745c6ebb9e19df398bccd21e225
edgar <edgar.rojasu@udea.edu.co>
2026-04-19 01:11:35 -0500

    creacion de poyecto qt y clase estadisticas de jugador

---
+++ desafio/lectorcsv.cpp
@@ -0,0 +1,3 @@
+#include "lectorcsv.h"
+
+LectorCSV::LectorCSV() {}
---
+++ desafio/lectorcsv.h
@@ -0,0 +1,12 @@
+#ifndef LECTORCSV_H
+#define LECTORCSV_H
+
+#include "lectorCSV_global.h"
+
+class LECTORCSV_EXPORT LectorCSV
+{
+public:
+    LectorCSV();
+};
+
+#endif // LECTORCSV_H
